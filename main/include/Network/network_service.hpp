b5740dd928f5d814a3bb859011323ca36a9717a6
Rafael Andrade <rafael.andrade@42we.tech>
2026-04-15 18:40:34 -0300

    Add ping all devices

--- main/include/Network/network_service.hpp
+++ main/include/Network/network_service.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
+#include <cstdint>
 namespace service::network {
+
+enum class RxCommand : uint8_t {
+    ACK = 0,
+    PING,
+};
+
+enum class TxCommand : uint8_t {
+    ACK = 0,
+    PING,
+};
 
 void init();
 void handler();
--- main/src/Network/network_service.cpp
+++ main/src/Network/network_service.cpp
@@ -1,4 +1,5 @@
 #include "Network/network_service.hpp"
+#include "Network/esp_now_driver.hpp"
 
 #include <array>
 #include <cstdint>
@@ -7,12 +8,21 @@ namespace service::network {
 
 static constexpr uint8_t MAX_DEVICE_TABLE = 10;
 
+static constexpr uint8_t HEADER_CMD = 1;
+
 static std::array<uint32_t, MAX_DEVICE_TABLE> devices;
 
 void init() {}
 
 void handler() {}
 
-void ping_all_devices_connected() {}
+void ping_all_devices_connected() {
+
+    std::array<uint8_t, HEADER_CMD> data = {
+        ESP
+    }
+
+    driver::network::esp_now::send_broadcast(const uint8_t *data, size_t len)
+}
 
 } // namespace service::network
